c26ab34077922802397befe249969af7bad633dc
dongwei <ye_dongwei@hotmail.com>
2023-02-16 21:17:37 +0100

    add GP

--- main.py
+++ main.py
@@ -2,25 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gpytorch
 import torch
-
-class ExactGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
-        self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
-
-    def forward(self, x):
-        mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
-
+import GPy
 
 
 ### fix seed
 np.random.seed(10)
 
 ### Parameters
-num_exact = 20
+num_exact = 14
 num_vague = 1
 
 ### Create synthetic data for example
@@ -41,24 +30,26 @@ yvague_gt = np.sin(Xvague_gt)*Xvague_gt/2
 random_bias = np.random.rand(num_vague)
 std = 0.5
 vague_X_distribution = 1/np.sqrt(2*np.pi)*std*np.exp(-0.5*((X-Xvague_gt-random_bias)/std)**2)
-print(vague_X_distribution)
+# print(vague_X_distribution)
 
 
 ### Train a GP to find the hyperparameters for the kernel in ELBO
-likelihood = gpytorch.likelihoods.GaussianLikelihood()
-model = ExactGPModel(Xexact, yexact, likelihood)
+kernel = GPy.kern.RBF(input_dim=1, variance=10., lengthscale=1.)
+GP = GPy.models.GPRegression(np.expand_dims(Xexact,axis=1), np.expand_dims(yexact,axis=1), kernel)
+GP.Gaussian_noise.fix(1e-4)
+GP.optimize(messages=False, max_f_eval=1, max_iters=1e7)
+GP.optimize_restarts(num_restarts=3)
+y_pred, y_pred_var = GP.predict(np.expand_dims(X,axis=1))
 
-model.train()
-likelihood.train()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
 
 ### Visualization
-plt.plot(X,y,'k-',label='groundtruth')
-plt.plot(Xexact,yexact,'r*',label='exact data')
-plt.plot(Xvague_gt,yvague_gt,'b*',label='vague data ground truth')
-
+plt.plot(X,y,'k-',linewidth=3,label='groundtruth')
+plt.plot(Xexact,yexact,'r*',markersize=10,label='exact data')
+plt.plot(Xvague_gt,yvague_gt,'b*',markersize=10,label='vague data ground truth')
 plt.plot(X,10*vague_X_distribution+yvague_gt,'g-',label='vague distribution')
+plt.plot(X,y_pred,linewidth=3,color='tab:purple',label='GP prediction (exact)')
 
 plt.legend()
 plt.show()
