fix the error in prior expression. Extend the code to general case with multiple uncertain data. Add noise variance to prpblem. But not done yet

include/func.py
@@ -4,17 +4,23 @@ class FuncClass():
     def __init__(self, function_name):
         self.function_name = function_name
 
-    def run(self,input):
+    def run(self,input,noise_variance):
         if self.function_name=='ampsin':
-            return np.sin(input)*input/2
+            result = np.sin(input)*input/2
         elif self.function_name=='ampcos':
-            return np.cos(input)*input/2
+            result = np.cos(input)*input/2
         elif self.function_name=='exp-sin':
-            return np.exp(-0.2*input)*np.sin(input)
+            result = np.exp(-0.2*input)*np.sin(input)
         elif self.function_name=='tan-sin':
-            return np.tan(0.15*input)*np.sin(input)
+            result = np.tan(0.15*input)*np.sin(input)
         elif self.function_name=='tanh-cos':
-            return 0.2*np.square(input)*np.tanh(np.cos(input)) 
+            result = 0.2*np.square(input)*np.tanh(np.cos(input)) 
         elif self.function_name=='log-sin':
-            return 0.5*np.log(np.square(input)*(np.sin(2*input)+2)+1)
+            result = 0.5*np.log(np.square(input)*(np.sin(2*input)+2)+1)
+        
+        if noise_variance != False:
+            print(result.shape)
+            result = result + np.random.normal(0,noise_variance,result.shape[0])
+
+        return result
 

include/mcmc.py
@@ -6,8 +6,8 @@ def bind_data(prior_mean,prior_variance,xexact,yexact,yvague,kernel):
     return binded_data
 
 
-
 ### Multivariate prior
+### The setting is incorrect (mixed up the multivariate GP with independent prior of each points)
 def prior_function_mul(input_vector,mean_vector,covariance_matrix):
     num_vague = input_vector.shape[1]
     return 1/np.sqrt((2*np.pi)**(num_vague)*np.linalg.det(covariance_matrix))* \
@@ -15,15 +15,21 @@ def prior_function_mul(input_vector,mean_vector,covariance_matrix):
                         np.linalg.inv(covariance_matrix)@\
                         (input_vector-np.expand_dims(mean_vector,axis=0)).T)
 
-### Univariate prior
+### Univariate prior, default setting for examples (1D input)
+### But always suitable for multiple uncertain points. Joint prior distribution = p(x_1)*p(x_2)...*p(x_{numvague})
+
 def prior_function_uni(input_vector,mean_vector,variance_vector):
-    return 1/np.sqrt((2*np.pi)*variance_vector)* \
+    prior_array = 1/np.sqrt((2*np.pi)*variance_vector)* \
             np.exp(-0.5*(input_vector-np.squeeze(mean_vector))**2/np.squeeze(variance_vector))
+    return np.prod(prior_array)
+
+
 
 ### databinding includes : 1. Prior information -> mean and variance
 ###                        2. Exact information -> xexact, yexact, yvague (belong to vague points but reflect the functions)
 ###                        3. GP information -> kernel
-def Metropolis_Hasting(timestep,initial_sample,assumption_variance,databinding):
+### Currently based on Metroplis method assuming the distribution is symmetric
+def Metropolis_Hasting(timestep,initial_sample,assumption_variance,databinding,noise_output):
 
     ### Release databinding
     Xvague_prior_mean = databinding[0]
@@ -42,32 +48,41 @@ def Metropolis_Hasting(timestep,initial_sample,assumption_variance,databinding):
 
     ### MCMC sampling
     for t in range(timestep):
-
-        ### Important! The workflow below this is now univaraite!!!
+        # print('Timestep:',t, end='\r')
+        ### Important! The workflow below this is now univaraite!!! output [sample_size->1,num_vague]
         if num_vague == 1:
-            x_new = np.abs(np.random.normal(np.squeeze(xvague_sample_current),assumption_variance,1))
+            x_new = np.random.normal(np.squeeze(xvague_sample_current),assumption_variance,1)
         else:
-            x_new = np.abs(np.random.multivariate_normal(np.squeeze(xvague_sample_current),\
-                                                    np.identity(num_vague)*assumption_variance,1))
+            x_new = np.random.multivariate_normal(np.squeeze(xvague_sample_current),\
+                                                    np.identity(num_vague)*assumption_variance,1)
+        x_new[x_new<0] = 0
+        
+        prior_function_upper = prior_function_uni(x_new,Xvague_prior_mean,Xvague_prior_var)
+        prior_function_lower = prior_function_uni(xvague_sample_current,Xvague_prior_mean,Xvague_prior_var)
 
         ### Component to compute multivariate Gaussian function for prior
-        if num_vague == 1:
-            prior_function_upper = prior_function_uni(x_new,Xvague_prior_mean,Xvague_prior_var)
-            prior_function_lower = prior_function_uni(xvague_sample_current,Xvague_prior_mean,Xvague_prior_var)
-        else:
-            ### Define an independent for prior covariance matrix
-            Xvague_prior_cov = np.diag(Xvague_prior_var)
-            prior_function_upper = prior_function_mul(x_new,Xvague_prior_mean,Xvague_prior_cov)
-            prior_function_lower = prior_function_mul(xvague_sample_current,Xvague_prior_mean,Xvague_prior_cov)
+        # if num_vague == 1:
+        #     prior_function_upper = prior_function_uni(x_new,Xvague_prior_mean,Xvague_prior_var)
+        #     prior_function_lower = prior_function_uni(xvague_sample_current,Xvague_prior_mean,Xvague_prior_var)
+        # else:
+        #     ### Define an independent for prior covariance matrix
+        #     Xvague_prior_cov = np.diag(Xvague_prior_var)
+        #     prior_function_upper = prior_function_mul(x_new,Xvague_prior_mean,Xvague_prior_cov)
+        #     prior_function_lower = prior_function_mul(xvague_sample_current,Xvague_prior_mean,Xvague_prior_cov)
         
-        ### Component to compute multivariate Gaussian function for likelihood, note here it is also noice free
+        ### Component to compute multivariate Gaussian function for likelihood (currently noice-free)
         x_upper = np.append(Xexact,x_new)
         x_lower = np.append(Xexact,xvague_sample_current)
-
         y_vector = np.append(yexact,yvague)
 
-        covariance_upper = kernel.K(np.expand_dims(x_upper,axis=1))
-        covariance_lower = kernel.K(np.expand_dims(x_lower,axis=1))
+        if noise_output == False:
+            print(kernel)
+            covariance_upper = kernel.K(np.expand_dims(x_upper,axis=1))
+            covariance_lower = kernel.K(np.expand_dims(x_lower,axis=1))
+            print(covariance_upper.shape,covariance_lower.shape)
+        else:
+            covariance_upper = kernel.K(np.expand_dims(x_upper,axis=1)) 
+            covariance_lower = kernel.K(np.expand_dims(x_lower,axis=1)) 
 
         determinant_upper = np.linalg.det(covariance_upper)
         determinant_lower = np.linalg.det(covariance_lower)
@@ -90,7 +105,6 @@ def Metropolis_Hasting(timestep,initial_sample,assumption_variance,databinding):
         else:
             pass
             # print('Accept ratio: ',accept_ratio,'; Xnew: ',x_new,'; Reject')
-    
     
     
     # ### Truncate 1/4 of burning-in period sample

main.py
@@ -12,30 +12,32 @@ from include.GP import *
 from include.mcmc import *
 
 ### fix seed
-np.random.seed(10)
+np.random.seed(4)
 
 ### Data parameters for the experiment
-func = FuncClass('log-sin')      # Define test function
+output_noise_variance = False
+func = FuncClass('ampsin')      # Define test function
+
 dim = 1                         # Define the dimension of the problem (not encoded))
-num_exact = 13                 # number of exact training data
-num_vague = 2                  # number of vague training datas
+num_exact = 20                 # number of exact training data
+num_vague = 2                 # number of vague training datas
 xscale = 8*np.pi                # Scale of input space (array if dim!=1)
 prior_var = 1
 num_pred = 3
 
 ### Create groundtruth data for visualization
 X = np.arange(0,xscale,xscale/100)
-y = func.run(X)
+y = func.run(X,False)
 
 ### Create synthetic data 
 Xexact = np.random.rand(num_exact)*xscale ### exact training data
-yexact = func.run(Xexact)
+yexact = func.run(Xexact,output_noise_variance)
 
 ### Create a bunch of vague training data (groundtruth) 
 Xvague_gt = np.random.rand(num_vague)*xscale
-yvague_gt = func.run(Xvague_gt)
+yvague_gt = func.run(Xvague_gt,output_noise_variance)
 
-### Create the prior of the vague data
+### Create the prior of the vague data, isotropic varianace 
 Xvague_prior_mean = Xvague_gt+np.random.rand(num_vague)
 Xvague_prior_var = np.ones(num_vague)*prior_var# May result in error when num_vague!=1
 
@@ -43,28 +45,29 @@ Xvague_prior_var = np.ones(num_vague)*prior_var# May result in error when num_va
 # visual_preprocess(X,y,Xexact,yexact,Xvague_gt,yvague_gt,show=True,save=False)
 
 ### Train a GP to find the hyperparameters for the kernel in ELBO
-preGP = GP('rbf',noise=1e-4,gp_lengthscale=1,gp_variance=10,message=False,restart_num=2)
+preGP = GP('rbf',output_noise=output_noise_variance,gp_lengthscale=1,gp_variance=10,message=False,restart_num=2)
 preGP.train(np.expand_dims(Xexact,axis=1),np.expand_dims(yexact,axis=1))
 y_pred,y_var = preGP.predict(np.expand_dims(X,axis=1))
 
+estimated_noise = preGP.get_noise()
+print(estimated_noise)
 visual_prediction(X,y,Xexact,yexact,Xvague_gt,yvague_gt,y_pred,show=True,save=False)
 
 ### Posterior distribution of input point distributions with MCMC
 ### With MCMC, samples of posterior distribution wil be genenrated
-### As we know the posterior is Gaussian, we derive mean and variance
 
 ### Settings for MCMC
 ### Let the initial guess to be mean of of the prior to each vague data points
-# xvague_sample_current = 10*np.ones(num_vague).reshape(1,-1)            ### Initial samples for each datapoints
-xvague_sample_current = np.multiply(Xvague_prior_mean,np.ones(num_vague)).reshape(1,-1)            ### Initial samples for each datapoints
-assumption_variance = 3                                               ### Assumption variance for jump distribution can not be too small as this will define the searching area
-timestep = 5000                                                        ### Artificial timestep
+### Initial samples for each datapoints
+xvague_sample_current = np.multiply(Xvague_prior_mean,np.ones(num_vague)).reshape(1,-1)
+assumption_variance = 0.3            ### Assumption variance for jump distribution can not be too small as this will define the searching area
+timestep = 1                   ### Artificial timestep
 
 ### Bind data for MH computing
 databinding = bind_data(Xvague_prior_mean,Xvague_prior_var,Xexact,yexact,yvague_gt,preGP.kernel)
 
 ### Perform MCMC with MH algorithm
-Xvague_posterior_samplelist = Metropolis_Hasting(timestep,xvague_sample_current,assumption_variance,databinding)
+Xvague_posterior_samplelist = Metropolis_Hasting(timestep,xvague_sample_current,assumption_variance,databinding,output_noise_variance)
 Xvague_posterior_mean = np.mean(Xvague_posterior_samplelist,axis=0)
 Xvague_posterior_variance = np.var(Xvague_posterior_samplelist,axis=0)
 
@@ -75,28 +78,28 @@ print('Groundtruth:                             ',Xvague_gt)
 ### Visualization of prior, posterior(samples) and groundtruth
 plot_distribution(Xvague_prior_mean,Xvague_prior_var,Xvague_posterior_samplelist,Xvague_gt)
 
-### Derive marginalized the posterior distribution over posterior 
-### Two way to implmement from this (the posterior of the input locations)
-###   1) Directly use the samples from MCMC (currently using this!!!!)
-###   2) Computer Gaussian distribution and sample from it
+# ### Derive marginalized the posterior distribution over posterior 
+# ### Two way to implmement from this (the posterior of the input locations)
+# ###   1) Directly use the samples from MCMC (currently using this!!!!)
+# ###   2) Computer Gaussian distribution and sample from it
 
-GPvariance,GPlengthscale = preGP.get_parameter()
-# ### Generate saving lists for prediction
-y_final_mean_list = np.empty((0,X.shape[0]))
-y_final_var_list = np.empty((0,X.shape[0]))
+# GPvariance,GPlengthscale = preGP.get_parameter()
+# # ### Generate saving lists for prediction
+# y_final_mean_list = np.empty((0,X.shape[0]))
+# y_final_var_list = np.empty((0,X.shape[0]))
 
-# ### Now retrain the GP with new position
-for i in range(1):
-# for i in range(Xvague_posterior_samplelist.shape[0]):
-      local_xtrain = np.append(Xexact,Xvague_posterior_samplelist[i,:])
-      local_ytrain = np.append(yexact,yvague_gt)
+# # ### Now retrain the GP with new position
+# for i in range(1):
+# # for i in range(Xvague_posterior_samplelist.shape[0]):
+#       local_xtrain = np.append(Xexact,Xvague_posterior_samplelist[i,:])
+#       local_ytrain = np.append(yexact,yvague_gt)
 
-      postGP = GP('rbf',noise=1e-4,gp_lengthscale=GPlengthscale,gp_variance=GPvariance,message=False,restart_num=5)
-      postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1))
-      y_final_mean,y_final_var = postGP.predict(np.expand_dims(X,axis=1))
+#       postGP = GP('rbf',noise=1e-4,gp_lengthscale=GPlengthscale,gp_variance=GPvariance,message=False,restart_num=5)
+#       postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1))
+#       y_final_mean,y_final_var = postGP.predict(np.expand_dims(X,axis=1))
       
-      y_final_mean_list = np.vstack((y_final_mean_list,y_final_mean.T))
-      y_final_var_list = np.vstack((y_final_var_list,y_final_var.T))
+#       y_final_mean_list = np.vstack((y_final_mean_list,y_final_mean.T))
+#       y_final_var_list = np.vstack((y_final_var_list,y_final_var.T))
 
-visual_uncertainty(X,y,Xvague_gt,yvague_gt,y_pred,y_var,np.mean(y_final_mean_list,axis=0),np.mean(y_final_var_list,axis=0),show=True,save=False)
+# visual_uncertainty(X,y,Xvague_gt,yvague_gt,y_pred,y_var,np.mean(y_final_mean_list,axis=0),np.mean(y_final_var_list,axis=0),show=True,save=False)
 