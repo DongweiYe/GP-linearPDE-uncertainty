fix the bug in visualization. variance->std and prior/posterior bug. First two examples set up

include/func.py
@@ -10,7 +10,7 @@ class FuncClass():
         elif self.function_name=='ampcos':
             result = np.cos(input)*input/2
         elif self.function_name=='exp-sin':
-            result = np.exp(-0.2*input)*np.sin(input)
+            result = np.exp(-0.2*input)*(np.sin(input)+input)
         elif self.function_name=='tan-sin':
             result = np.tan(0.15*input)*np.sin(input)
         elif self.function_name=='tanh-cos':

include/vis.py
@@ -3,29 +3,41 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import seaborn as sns
 
-def visual_preprocess(x,y,xexact,yexact,xvague,yvague,show=False,save=False):
+def visual_data(x,y,xexact,yexact,xvague,xvague_prior_mean,yvague,show=False,save=False):
+    plt.figure(figsize=(15, 3))
     plt.plot(x,y,'k-',linewidth=3,label='groundtruth')
-    plt.plot(xexact,yexact,'r*',markersize=10,label='exact data')
-    plt.plot(xvague,yvague,'b*',markersize=10,label='vague data ground truth')
+    plt.plot(xexact,yexact,'ro',markersize=10,label='fixed data')
+    plt.plot(xvague,yvague,'bo',markersize=10,label='uncertain data ground truth')
+    plt.plot(xvague_prior_mean,yvague,'go',markersize=10,label='uncertain data prior mean')
     # plt.plot(X,10*vague_X_distribution+yvague_gt,'g-',label='vague distribution')
-    plt.legend()
+    
+    for i in range(xvague.shape[0]):
+        plt.arrow(xvague[i],yvague[i],xvague_prior_mean[i]-xvague[i],0,color='grey')
+    
+    # plt.legend()
     if show == True:
         plt.show()
     if save == True:
         plt.savefig('preprocess.png',bbox_inches='tight')
 
 
-def visual_GP(x,y,xexact,yexact,xvague,yvague,ypred,show=False,save=False):
+def visual_GP(x,y,xexact,yexact,xvague,xvague_prior_mean,yvague,ypred,yvar,show=False,save=False):
+    plt.figure(figsize=(15, 3))
+
     plt.plot(x,y,'k-',linewidth=3,label='groundtruth')
-    plt.plot(xexact,yexact,'r*',markersize=10,label='exact data')
-    plt.plot(xvague,yvague,'b*',markersize=10,label='vague data ground truth')
-    # plt.plot(X,10*vague_X_distribution+yvague_gt,'g-',label='vague distribution')
-    plt.plot(x,ypred,linewidth=3,color='tab:purple',label='GP prediction (exact)')
-    plt.legend()
+    plt.plot(xexact,yexact,'ro',markersize=10,label='fixed data')
+    plt.plot(xvague,yvague,'bo',markersize=10,label='uncertain data ground truth')
+    plt.plot(xvague_prior_mean,yvague,'go',markersize=10,label='uncertain data prior mean')
+    
+    plt.plot(x,ypred,linewidth=3,color='tab:purple',label='GP prediction direct')
+    plt.fill_between(x,np.squeeze(ypred-np.sqrt(yvar)),np.squeeze(ypred+np.sqrt(yvar)),color='tab:purple',alpha=0.3)
+    for i in range(xvague.shape[0]):
+        plt.arrow(xvague[i],yvague[i],xvague_prior_mean[i]-xvague[i],0,color='grey')
+    # plt.legend()
     if show == True:
         plt.show()
     if save == True:
-        plt.savefig('prediction.png',bbox_inches='tight')
+        plt.savefig('GPprediction.png',bbox_inches='tight')
 
 
 def vis_uncertain_input(x,y,xvague,xvague_prior_mean,xvague_post_mean,yvague,show=False,save=False):
@@ -47,28 +59,31 @@ def vis_prediction(x,y,\
                    prior_ypred_list,prior_yvar_list,\
                     post_ypred_list,post_yvar_list,show=False,save=False):
     postymean_mean = np.mean(post_ypred_list,axis=0)
-    postymean_std = np.std(post_ypred_list,axis=0)
+    postymean_var = np.var(post_ypred_list,axis=0)
     postyvar_mean = np.mean(post_yvar_list,axis=0)
     # postyvar_std = np.std(post_yvar_list,axis=0)
 
-    post_lower_bound = np.squeeze(postymean_mean-postymean_std-postyvar_mean)
-    post_upper_bound = np.squeeze(postymean_mean+postymean_std+postyvar_mean)
+    post_lower_bound = np.squeeze(postymean_mean-np.sqrt(postymean_var+postyvar_mean))
+    post_upper_bound = np.squeeze(postymean_mean+np.sqrt(postymean_var+postyvar_mean))
     
     priorymean_mean = np.mean(prior_ypred_list,axis=0)
-    priorymean_std = np.std(prior_ypred_list,axis=0)
+    priorymean_var = np.var(prior_ypred_list,axis=0)
     prioryvar_mean = np.mean(prior_yvar_list,axis=0)
     # prioryvar_std = np.std(prior_yvar_list,axis=0)
 
-    prior_lower_bound = np.squeeze(priorymean_mean-priorymean_std-prioryvar_mean)
-    prior_upper_bound = np.squeeze(priorymean_mean+priorymean_std+prioryvar_mean)
+    prior_lower_bound = np.squeeze(priorymean_mean-np.sqrt(priorymean_var+prioryvar_mean))
+    prior_upper_bound = np.squeeze(priorymean_mean+np.sqrt(priorymean_var+prioryvar_mean))
 
+    plt.figure(figsize=(15, 3))
     plt.plot(x,y,'k-',linewidth=3,label='groundtruth')
 
-    plt.plot(x,postymean_mean,'b--',linewidth=3,label='with prior')
-    plt.fill_between(x,post_lower_bound,post_upper_bound,color='blue',alpha=0.3)
+    plt.plot(x,priorymean_mean,'--',color='tab:blue',linewidth=3,label='with prior')
+    plt.fill_between(x,prior_lower_bound,prior_upper_bound,color='tab:blue',alpha=0.3)
 
-    plt.plot(x,priorymean_mean,'r--',linewidth=3,label='with posterior')
-    plt.fill_between(x,prior_lower_bound,prior_upper_bound,color='red',alpha=0.3)
+    plt.plot(x,postymean_mean,'--',color='tab:red',linewidth=3,label='with posterior')
+    plt.fill_between(x,post_lower_bound,post_upper_bound,color='tab:red',alpha=0.3)
+
+    
 
     plt.legend()
     if show == True:

main.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import GPy
 
-import bayespy as bp ### Mainly for visualization
-import bayespy.plot as bpplt
-from scipy.stats import norm
+# import bayespy as bp ### Mainly for visualization
+# import bayespy.plot as bpplt
+from scipy.stats import qmc
 
 from include.func import *	
 from include.vis import *
@@ -15,43 +15,47 @@ from include.mcmc import *
 np.random.seed(5)
 
 ### Data parameters for the experiment
-output_noise_variance = 0.08
+output_noise_variance = 0.2
 func = FuncClass('log-sin')      # Define test function
 
 dim = 1                        # Define the dimension of the problem (not encoded))
-num_exact = 20                 # number of exact training data
-num_vague = 20                 # number of vague training datas
+num_exact = 30                 # number of exact training data
+num_vague = 30                 # number of vague training datas
 xscale = 8*np.pi               # Scale of input space (array if dim!=1)
 prior_var = 1
-num_pred = 3
+prior_bias_mean = 0.5
 
 ### Create groundtruth data for visualization
 X = np.arange(0,xscale,xscale/100)
 y = func.run(X,False)
 
-### Create synthetic data 
-Xexact = np.random.rand(num_exact)*xscale ### exact training data
+### Create synthetic data (sobol); 2^M
+qMCsampler = qmc.Sobol(d=dim)
+qMCsample = qMCsampler.random_base2(m=6)*xscale
+
+### fetch fixed data
+Xexact =  qMCsample[:num_exact,0] ### exact training data
 yexact = func.run(Xexact,output_noise_variance)
 
 ### Create a bunch of vague training data (groundtruth) 
-Xvague_gt = np.random.rand(num_vague)*xscale
+Xvague_gt = qMCsample[num_exact:(num_exact+num_vague),0]
 yvague_gt = func.run(Xvague_gt,output_noise_variance)
 
 ### Create the prior of the vague data, isotropic varianace 
-Xvague_prior_mean = Xvague_gt+np.random.rand(num_vague)
+Xvague_prior_mean = Xvague_gt+(np.random.rand(num_vague)*2-1)*prior_bias_mean
 Xvague_prior_var = np.ones(num_vague)*prior_var# May result in error when num_vague!=1
 
 ### Visualization of the problem
-# visual_preprocess(X,y,Xexact,yexact,Xvague_gt,yvague_gt,show=True,save=False)
+# visual_data(X,y,Xexact,yexact,Xvague_gt,Xvague_prior_mean,yvague_gt,show=False,save=True)
 
 ### Train a GP to find the hyperparameters for the kernel in ELBO
 preGP = GP('rbf',output_noise=output_noise_variance,gp_lengthscale=1,gp_variance=10,message=False,restart_num=2)
-preGP.train(np.expand_dims(np.append(Xexact,Xvague_prior_mean),axis=1),np.expand_dims(np.append(yexact,yvague_gt),axis=1))
+preGP.train(np.expand_dims(np.append(Xexact,Xvague_prior_mean),axis=1),np.expand_dims(np.append(yexact,yvague_gt),axis=1),1e-2)
 y_pred,y_var = preGP.predict(np.expand_dims(X,axis=1))
 
 estimated_noise = preGP.get_noise()
 print('Estimated noise: ', estimated_noise)
-# visual_GP(X,y,Xexact,yexact,Xvague_gt,yvague_gt,y_pred,show=True,save=False)
+visual_GP(X,y,Xexact,yexact,Xvague_gt,Xvague_prior_mean,yvague_gt,y_pred,y_var,show=True,save=False)
 
 ### Posterior distribution of input point distributions with MCMC
 ### With MCMC, samples of posterior distribution wil be genenrated
@@ -60,7 +64,7 @@ print('Estimated noise: ', estimated_noise)
 ### Let the initial guess to be mean of of the prior to each vague data points
 ### Initial samples for each datapoints
 xvague_sample_current = np.multiply(Xvague_prior_mean,np.ones(num_vague)).reshape(1,-1)
-assumption_variance = 0.3            ### Assumption variance for jump distribution can not be too small as this will define the searching area
+assumption_variance = 0.05            ### Assumption variance for jump distribution can not be too small as this will define the searching area
 timestep = 5000                   ### Artificial timestep
 
 ### Bind data for MH computing
@@ -74,10 +78,10 @@ Xvague_posterior_variance = np.var(Xvague_posterior_samplelist,axis=0)
 print('Posterioir mean and variance (Gaussian): ',Xvague_posterior_mean)
 print('Prior mean and variance (Gaussian):      ',Xvague_prior_mean)
 print('Groundtruth:                             ',Xvague_gt)
-
+print(Xvague_posterior_variance)
 ### Visualization of prior, posterior(samples) and groundtruth
 # plot_distribution(Xvague_prior_mean,Xvague_prior_var,Xvague_posterior_samplelist,Xvague_gt)
-
+vis_uncertain_input(X,y,Xvague_gt,Xvague_prior_mean,Xvague_posterior_mean,yvague_gt,show=True,save=False)
 
 ### Derive marginalized the predictive distribution over uncertain input posterior 
 ### Derive marginalized the predictive distribution over uncertain input prior (comparison) 
@@ -97,7 +101,7 @@ for i in range(Xvague_posterior_samplelist.shape[0]):
       local_ytrain = np.append(yexact,yvague_gt)
 
       postGP = GP('rbf',output_noise_variance,gp_lengthscale=GPlengthscale,gp_variance=GPvariance,message=False,restart_num=5)
-      postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1))
+      postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1),1e-2)
       y_final_mean,y_final_var = postGP.predict(np.expand_dims(X,axis=1))
       
       y_final_mean_list = np.vstack((y_final_mean_list,y_final_mean.T))
@@ -111,6 +115,7 @@ y_final_var_list_prior = np.empty((0,X.shape[0]))
 Xvague_prior_samplelist  = np.random.multivariate_normal(Xvague_prior_mean,np.identity(num_vague)*prior_var,\
                                                              Xvague_posterior_samplelist.shape[0])
 # print(Xvague_posterior_samplelist.shape)
+# for i in range(100):
 for i in range(Xvague_posterior_samplelist.shape[0]):
       
       ### Sample from prior
@@ -118,13 +123,11 @@ for i in range(Xvague_posterior_samplelist.shape[0]):
       local_ytrain = np.append(yexact,yvague_gt)
 
       postGP = GP('rbf',output_noise_variance,gp_lengthscale=GPlengthscale,gp_variance=GPvariance,message=False,restart_num=5)
-      postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1))
+      postGP.train(np.expand_dims(local_xtrain,axis=1),np.expand_dims(local_ytrain,axis=1),1e-2)
       y_final_mean,y_final_var = postGP.predict(np.expand_dims(X,axis=1))
       
       y_final_mean_list_prior = np.vstack((y_final_mean_list_prior,y_final_mean.T))
       y_final_var_list_prior = np.vstack((y_final_var_list_prior,y_final_var.T))
 
-
-# vis_uncertain_input(X,y,Xvague_gt,Xvague_prior_mean,Xvague_posterior_mean,yvague_gt,show=True,save=False)
 # visual_uncertainty(X,y,Xexact,yexact,Xvague_gt,Xvague_prior_mean,Xvague_posterior_mean,yvague_gt,y_pred,y_var,y_final_mean_list,y_final_var_list,show=True,save=False)
-vis_prediction(X,y,y_final_mean_list_prior,y_final_var_list_prior,y_final_mean_list,y_final_var_list,show=True,save=False)
+vis_prediction(X,y,y_final_mean_list_prior,y_final_var_list_prior,y_final_mean_list,y_final_var_list,show=False,save=True)