d7258d31bc0217276bc43a35c54f926f9dac5755
VhaoYan <w.yan@utwente.nl>
2024-12-13 15:22:42 +0100

    # changed keynum3

--- include/config.py
+++ include/config.py
@@ -1,5 +1,6 @@
 import jax
 
+# %%
 key_num = 100
 # key_num = 50
 key = jax.random.PRNGKey(key_num)
@@ -7,5 +8,5 @@ key = jax.random.PRNGKey(key_num)
 key_x_rd, key_t_rd, key_x_u, key_x_u_init, key_t_u, key_t_u_low, key_t_u_high, key_x_f, key_t_f, key_x_pred, \
                                                                     key_t_pred, key_x_noise, key_t_noise, \
                                                                     key = jax.random.split(key, 14)
-
+s
 
